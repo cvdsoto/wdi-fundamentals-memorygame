b4bc09b7906d3932779f041b17b54a54c86f16d0
Carmina <cvdsoto@gmail.com>
2020-01-23 11:09:55 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,24 @@
-let cardOne = "queen";
+/* let cardOne = "queen";
 let cardTwo = "king";
 let cardThree = "queen";
-let cardFour = "king";
+let cardFour = "king"; */
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+const cardOne = cards[0];
+const cardTwo = cards[2];
 
-console.log("User flipped " + cardOne);
-console.log ("User flipped " + cardTwo);
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
+
+/* console.log("User flipped " + cardOne);
+console.log ("User flipped " + cardTwo); */
